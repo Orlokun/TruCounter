6b8c04b895cca1b877c768ca0b76322dbdb46729
orlo <oguerrerofarias@gmail.com>
2021-03-11 19:37:07 -0300

    Next step DONATE!

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -4,35 +4,27 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
-<<<<<<< HEAD
     public static void FormatAndSaveShameData(ShameManager sManager)
-=======
-    public static void SaveImages(ShameDataManager sManager)
->>>>>>> f367fcff32ee6afe4e06792db0134f3ee7237185
     {
         BinaryFormatter bFormatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/shameData.shame";
+        string path = Application.persistentDataPath + "/shameBytes.dog";
 
         FileStream fStream = new FileStream(path, FileMode.Create);
 
-<<<<<<< HEAD
         bFormatter.Serialize(fStream, sManager.GetShameData());
-=======
-        bFormatter.Serialize(fStream, sManager);
->>>>>>> f367fcff32ee6afe4e06792db0134f3ee7237185
         fStream.Close();
     }
 
 
-    public static ShameDataManager LoadShameData()
+    public static ShameData LoadShameData()
     {
-        string path = Application.persistentDataPath + "/shameData.shame";
+        string path = Application.persistentDataPath + "/shameBytes.dog";
         if (File.Exists(path))
         {
             BinaryFormatter bFormatter = new BinaryFormatter();
             FileStream fStream = new FileStream(path, FileMode.Open);
 
-            ShameDataManager shameData = (ShameDataManager)bFormatter.Deserialize(fStream);
+            ShameData shameData = (ShameData)bFormatter.Deserialize(fStream);
             fStream.Close();
             return shameData;
         }
